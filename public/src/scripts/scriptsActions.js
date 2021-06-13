4bb516e944a5e879b03fda3ecc6a8d6f763f7172
Viviane de Souza <vivianesantosdevland@gmail.com>
2021-06-13 14:39:22 -0300

    Consegui aplicar o efeitor hover nas box, porém a opacidade do texto está na mesma porcentagem que a div.

---
+++ public/src/scripts/scriptsActions.js
@@ -0,0 +1,12 @@
+/*var textoHoverBox = document.getElementsById("efeito-hover");
+
+textoHoverBox.addEventListener("mousehover", function()
+{
+    if(textoHoverBox.style.display === "none")
+    {
+        textoHoverBox.style.display = "block";
+    }else{
+        textoHoverBox.style.display = "none"
+    }
+});
+*/

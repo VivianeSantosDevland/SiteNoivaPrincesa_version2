88d6c2ecb746136015565a3b5d6967699621ad01
Viviane de Souza <vivianesantosdevland@gmail.com>
2021-06-13 15:50:01 -0300

    Fazendo testes para o painel de fotos do topo. Falta automatizar a transição das fotos.

--- public/src/scripts/scriptsActions.js
+++ public/src/scripts/scriptsActions.js
@@ -10,3 +10,22 @@ textoHoverBox.addEventListener("mousehover", function()
     }
 });
 */
+
+function mudaFotoTopo(numero)
+{
+    var fotoTopo = document.getElementById("foto_topo");
+    
+    
+      /*"public\images\Vinhos-para-casamento-1180x517-o4tcpp4ey3g9fhsmzlxgjnyye63xpsmof9hlxwpl88.png"*/
+  
+    if(numero === 1)
+    {
+        fotoTopo.src = "../../images/Como-se-Legalizar-nos-EUA-Big-2.jpg";
+    }else if(numero === 2)
+    {
+        fotoTopo.src = "../../images/Vinhos-para-casamento-1180x517-o4tcpp4ey3g9fhsmzlxgjnyye63xpsmof9hlxwpl88.png";
+    }else
+    {
+        fotoTopo.src = "../../images/dicas-de-decoração-para-casamento-1024x300.jpg"
+    }
+}
